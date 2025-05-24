6516b245fa4d6a009105daf31c3dfa9b3f5ebeea
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-05-24 20:48:22 +0800

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+ 
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +8,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body> 
+        <p>Header</p> 
+        {children} 
+        <h1>Footer</h1>
+      </body>
     </html>
   );
 }
